e3cfd4f3f8b5c44101b6ce4c4de1063c71aea730
Hiroki Miura <hiroki-miura@outlook.jp>
2016-09-12 12:49:15 +0900

    ログイン機能追加２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 ﻿class BooksController < ApplicationController
+
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
-class User < ActiveRecord::Base
+﻿class User < ActiveRecord::Base
 has_many :reviews
+def self.authnticate(userid, password)
+    usr= find_by(userid: userid)
+    if usr != nil && usr.password == password then
+        usr
+    else
+        return
+    end
+ end
 end
